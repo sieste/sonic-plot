37bb89326c46442a26284589743fce9c8e91d907
sieste <stefan_siegert@gmx.de>
2016-04-03 13:13:36 +0100

    added roxygen documentation

--- R/sonify.R
+++ R/sonify.R
@@ -1,9 +1,39 @@
+#' Sonify data
+#'
+#' Synthesise a sound with frequency modulated by input data
+#'
+#' @param y The data values used to modulate the frequency.
+#' @param x The x values. Can be used when y values are unevenly spaced. Default is 1:length(y)
+#' @param waveform One of 'sine' (default), 'square', 'triangle', 'sawtooth'.
+#' @param duration Duration in seconds. Default is 2 seconds.
+#' @param amp_level Amplitude level between 0 and 1 to adjust the volume.
+#' @param stereo If TRUE (default) a left-to-right transition is simulated.
+#' @param smp_rate The sampling rate of the wav file, default: 44100 (CD quality)
+#' @param flim The frequency range to which the data is mapped. Default: [440, 880]
+#' @param play logical, whether the sound should be played. Default is TRUE. 
+#' @param player The program used to play the synthesized wave file. Default is 'mplayer' which is available under Linux.
+#' @param player_opts Additional options passed to tuneR::play. Default is ' > /dev/null 2> /dev/null' which suppresses mplayer's output and error messages.
+#'
+#' @return The synthesized sound as a tuneR::WaveMC object.
+#'
+#' @author Stefan Siegert \email{contact@@stefansiegert.net}
+#'
+#' @seealso tuneR::play, tuneR::WaveMC
+#' 
+#' @example
+#' x = seq(-3, 3, .01)
+#' y = dnorm(x)
+#' sonify(y)
+#'
+#' @export 
+
+
 library(tuneR)
 
 sonify = 
-function(x, y, waveform=c('sine', 'square', 'triangle', 'sawtooth'), 
+function(y, x=1:length(y), waveform=c('sine', 'square', 'triangle', 'sawtooth'), 
          duration=2, amp_level = 1,
-         stereo=TRUE, smp_rate=44100, flim=c(440, 880), na_freq=300, 
+         stereo=TRUE, smp_rate=44100, flim=c(440, 880), na_freq=300, play=TRUE,
          player='mplayer', player_opts=' > /dev/null 2> /dev/null')
 {
 
@@ -18,14 +48,13 @@ function(x, y, waveform=c('sine', 'square', 'triangle', 'sawtooth'),
   yr = range(y, na.rm=TRUE)
   yy = (y - yr[1]) / diff(yr) * diff(flim) + flim[1]
 
-
   # replace NA's by na_freq
   yy[is.na(yy)] <- na_freq
   
   # interpolate to length n
   yy = spline(x=x, y=yy, n=n)$y 
 
-  # fourier coefficients for waveform
+  # fourier coefficients for different waveform
   a = switch(waveform,
     sine = c(1, rep(0, 8)),
     square = c(1, 0, 1/3, 0, 1/5, 0, 1/7, 0, 1/9),
@@ -33,15 +62,14 @@ function(x, y, waveform=c('sine', 'square', 'triangle', 'sawtooth'),
     sawtooth = 1/(1:9)
   )
   
-  # waveform with instantaneous frequency yy
+  # create waveform with instantaneous frequency yy
   signal = rowSums(
     sapply(seq_along(a), function(i) {
       a[i] * sin(i * 2 * pi * cumsum(yy) / smp_rate)
     })
   )
   
-  # multiply amplitude and linear function to simulate movement from left to
-  # right
+  # multiply by linear function to simulate left-to-right transition
   if (stereo) {
     ramp = seq(0, 1, length=n)
   } else {
@@ -57,10 +85,8 @@ function(x, y, waveform=c('sine', 'square', 'triangle', 'sawtooth'),
   # synthesize
   play(final, player, player_opts)
 
-}
+  # return the synthesized WaveMC object
+  invisible(final)
 
-# the data to be sonified (standard normal distribution)
-x = seq(-3, 3, .01)
-y = dnorm(x)
-sonify(x, y)
+}
 
