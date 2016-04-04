765cd29cf06a4cb005ec35457d60787df4f51529
sieste <stefan_siegert@gmx.de>
2016-04-04 23:18:51 +0100

    robs changes
    - changed default settings
    - removed player options
    - different play() call between linux and windows
    - replaced some <- by =
    - IndicateNegatives = noise amplitude
    - ticks option as generalisation of robs "zero-crossing beep"
    - code indentation
    - more efficient finding of negative y values

--- Rsonify/sonify.R
+++ Rsonify/sonify.R
@@ -3,11 +3,11 @@
 #' Synthesise a sound with frequency modulated by input data
 #'
 #' @param y The data values used to modulate the frequency.
-#' @param x The x values. Can be used when y values are unevenly spaced. Default is 1:length(y)
+#' @param x The x values. Can be used when y values are unevenly spaced. Default is -length(y)/2:length(y)/2
 #' @param The waveform used for the sound. One of 'sine', 'square', 'triangle', 'sawtooth'. Default is 'sine'.
-#' @param pulse_len Length of individual pulses in seconds. Default is 0.
+#' @param pulse_len Length of individual pulses in seconds. Default is 0.1.
 #' @param pulse_amp Amplitude of pulses between 0 and 1. Default is 0.2.
-#' @param duration Total duration in seconds. Default is 2.
+#' @param duration Total duration in seconds. Default is 5.
 #' @param amp_level Amplitude level between 0 and 1 to adjust the volume. Default is 1.
 #' @param stereo If TRUE a left-to-right transition is simulated. Default is TRUE.
 #' @param smp_rate The sampling rate of the wav file. Default is 44100 (CD quality)
@@ -15,7 +15,6 @@
 #' @param na_freq Frequency in Hz that is used for NA data. Default is 300.
 #' @param play If TRUE, the sound is played. Default is TRUE. 
 #' @param player The program used to play the synthesized wave file. Default is 'mplayer' which is available under Linux. Under windows, try player='mplay32.exe' or player='wmplayer.exe'.
-#' @param player_opts Additional options passed to tuneR::play. Default is ' > /dev/null 2> /dev/null' which suppresses mplayer's output and error messages. Under windows, try player_opts='/play /close'.
 #'
 #' @return The synthesized sound saved as a tuneR::WaveMC object.
 #' 
@@ -37,46 +36,71 @@
 library(tuneR)
 
 sonify = 
-function(y, x=1:length(y), 
+function(y, x=seq_along(y)-round(length(y)/2),
          waveform=c('sine', 'square', 'triangle', 'sawtooth'), 
-         pulse_len=0, pulse_amp=0.2,
-         duration=2, amp_level = 1,
-         stereo=TRUE, smp_rate=44100, flim=c(440, 880), na_freq=300, play=TRUE,
-         player='mplayer', player_opts=' > /dev/null 2> /dev/null')
+         ticks = 0, tick_len = 0.05, 
+         pulse_len=0.1, pulse_amp=0.2,
+         duration=5, amp_level = 1, IndicateNegatives=0.5,
+         stereo=TRUE, smp_rate=44100, flim=c(440, 880), na_freq=300, play=TRUE)
 {
 
   # error checking
   flim = sort(flim)
+  ticks = sort(ticks)
   waveform = match.arg(waveform)
 
-  # total number of samples
+  # auxiliary quantities
   n = duration * smp_rate
+  y_ran = range(y, na.rm=TRUE)
+  x_ran = range(x, na.rm=TRUE)
+
+  # function to make signal
+  MakeSignal <- function(yy, waveform=waveform, ...) {
+    # fourier coefficients for different waveform
+    a = switch(waveform,
+      sine = 1,
+      square = c(1, 0, 1/3, 0, 1/5, 0, 1/7, 0, 1/9),
+      triangle = c(1, 0, -1/9, 0, 1/25, 0, -1/49, 0, 1/81),
+      sawtooth = 1/(1:9)
+    )
+    # create waveform with instantaneous frequency yy
+    sig = rowSums(
+    sapply(seq_along(a), function(i) {
+        a[i] * sin(i * 2 * pi * cumsum(yy) / smp_rate)
+      })
+    )
+    return(sig)
+  }
+  
   
   # rescale y values to desired frequency range 
-  yr = range(y, na.rm=TRUE)
-  yy = (y - yr[1]) / diff(yr) * diff(flim) + flim[1]
+  yy = (y - y_ran[1]) / diff(y_ran) * diff(flim) + flim[1]
 
   # replace NA's by na_freq
-  yy[is.na(yy)] <- na_freq
+  yy[is.na(yy)] = na_freq
   
   # interpolate to length n
-  yy = spline(x=x, y=yy, n=n)$y 
-
-  # fourier coefficients for different waveform
-  a = switch(waveform,
-    sine = 1,
-    square = c(1, 0, 1/3, 0, 1/5, 0, 1/7, 0, 1/9),
-    triangle = c(1, 0, -1/9, 0, 1/25, 0, -1/49, 0, 1/81),
-    sawtooth = 1/(1:9)
-  )
+  interp = spline(x=x, y=yy, n=n)
+  xx = interp$x
+  yy = interp$y
   
-  # create waveform with instantaneous frequency yy
-  signal = rowSums(
-    sapply(seq_along(a), function(i) {
-      a[i] * sin(i * 2 * pi * cumsum(yy) / smp_rate)
-    })
-  )
+  # make signal for range of x and yy
+  signal = MakeSignal(yy, waveform=waveform)
 
+  # indicate ticks by sawtooth burst
+  n_tick_half = round(tick_len * smp_rate / 2)    
+  for (i in seq_along(ticks)) {
+    tick_ = ticks[i]
+print(tick_)
+    if(tick_ > x_ran[1] & tick_ < x_ran[2]) {
+      # ind is largest index smaller than tick index
+      ind = which.max(xx[xx < tick_])
+      xinds = (ind - n_tick_half):(ind + 1 + n_tick_half)
+      xinds = xinds[xinds > 0 & xinds <= n]
+      signal[xinds] = MakeSignal(yy[xinds], waveform='sawtooth')
+    }
+  }
+    
   # add pulses
   if (pulse_len > 0) {
     n_pulse_half = round(pulse_len * smp_rate / 2)
@@ -87,7 +111,15 @@ function(y, x=1:length(y),
       signal[j] = signal[j] + pulse_amp * runif(1)
     }
   }
-  
+
+  # add white noise to negative values of signal if applicable
+  if(IndicateNegatives > 0 & y_ran[1] < 0) {
+    # find negative values of interpolated y
+    transf_zero = flim[1] + (-y_ran[1] / diff(y_ran)) * diff(flim)
+    Negatives = which(yy < transf_zero)
+    signal[Negatives] <- signal[Negatives] + IndicateNegatives * runif(length(Negatives))
+  }
+    
   # multiply by linear function to simulate left-to-right transition
   if (stereo) {
     ramp = seq(0, 1, length=n)
@@ -103,11 +135,14 @@ function(y, x=1:length(y),
   
   # synthesize
   if (play) {
-    play(final, player, player_opts)
+    if (Sys.info()[['sysname']] == 'Linux') {
+      play(final, 'mplayer', ' > /dev/null 2> /dev/null')
+    } else {
+      play(final)
+    }
   }
 
   # return the synthesized WaveMC object
   invisible(final)
 
 }
-
